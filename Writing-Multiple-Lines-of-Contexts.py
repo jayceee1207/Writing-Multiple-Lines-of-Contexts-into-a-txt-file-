9750bbc60282e8c67b8214671393fc66cd35f042
jayceee1207 <ablayjc7@gmail.com>
2023-04-25 08:52:35 +0800

    Pseudocode

--- Writing-Multiple-Lines-of-Contexts.py
+++ Writing-Multiple-Lines-of-Contexts.py
@@ -2,3 +2,22 @@
 #BSCPE 1-5
 #Assignment 4: Program 3
 #April 22, 2023
+
+#make a file where we will store random numbers
+
+#read every line of number file
+
+#create a list where we will store all squared even numbers
+#create a list where we will store all cubed odd numbers
+
+#make a for loop in number file:
+#   convert number into an integer
+#   if number divided by 2 is equal to 0
+#       number raise to 2
+#       add the number to squared even numbers list
+#   if number divided by 2 is not equal to 0
+#       number raise to 3
+#       add the number to cubed odd numbers list
+
+#open a file to store all numbers in the squared even numbers list
+#open a file to store all numbers in the cubed odd numbers list
