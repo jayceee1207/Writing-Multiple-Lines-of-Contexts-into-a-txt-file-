030a127003408465906e60593ab28992a109b661
jayceee1207 <ablayjc7@gmail.com>
2023-04-25 09:50:07 +0800

    Function name

--- Writing-Multiple-Lines-of-Contexts.py
+++ Writing-Multiple-Lines-of-Contexts.py
@@ -12,8 +12,10 @@ print("*********************************************************")
 #PSEUDOCODE
 
 #make a function that will make every line and upload to the other file
-
+def lines():
 #open file named myfile.txt
+    my_file = open("myfile.txt", "w")
+    line = 'y' or 'Y'
 #while line is equal to y
 #   ask the user to input in the line
 #   write on the line
